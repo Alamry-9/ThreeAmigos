cd8fca388688daf8305b4af71a2eb79ac5675ae7
Leszek <leszek@ambiesense.com>
2021-02-22 14:59:38 +0000

    Adding Brand and Category to Products

--- ThreeAmigos.Persistance/Configurations/ProductEntityTypeConfiguration.cs
+++ ThreeAmigos.Persistance/Configurations/ProductEntityTypeConfiguration.cs
@@ -9,12 +9,13 @@ namespace ThreeAmigos.Persistance.Configurations
         public void Configure(EntityTypeBuilder<Product> builder)
         {
             builder.HasKey(es => es.IdProduct);
-            /*
-            builder.HasOne(es => es.Product)
-                   .WithMany(s => s.StoreProduct)
-                   .HasForeignKey(es => es.IdProduct);
-            */
-            
+
+            builder.HasOne(es => es.Brand);
+            builder.HasOne(es => es.Category);
+
+
+
+
         }
     }
 }
